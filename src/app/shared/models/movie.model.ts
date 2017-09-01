622cd13ca7161d41019697d6becac2f4947f0f34
OgnjenPetrovic <zoomght@gmail.com>
2017-09-01 20:06:37 +0200

    Drugi zadatak

---
+++ src/app/shared/models/movie.model.ts
@@ -0,0 +1,14 @@
+export class Movies {
+    
+        constructor (
+            public id?: number,
+            public name?: string,
+            public director?: string,
+            public imageUrl?: string,
+            public duration?: number,
+            public releaseData?: string,
+            public genres?: string [],
+        ){}
+        
+    }
+    
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MovieService } from './services/movie.service';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  
+  declarations: [ 
+    //
+  ],
+  providers: [
+    MovieService
+  ],
+
+  exports: [
+  	//
+  ]
+
+})
+
+export class SharedModule { }
